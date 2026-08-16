b3f08645abaf02eb94689793086c2f274b2c0d76
shithil008 <32437885+shithil008@users.noreply.github.com>
2026-08-17 02:02:50 +0600

    Enhance CSPDisabler with new features and hooks
    Refactor CSPDisabler to improve functionality and add periodic cleanup.

--- tampermonkey/no-csp.js
+++ tampermonkey/no-csp.js
@@ -1,229 +1,137 @@
 // no-csp.js
-// Load via @require in Tampermonkey/Violentmonkey/Greasemonkey
-
 (function(global) {
     'use strict';
 
     const CSPDisabler = {
         initialized: false,
         observer: null,
+        cleanupTimer: null,
         options: {},
 
         init: function(options) {
             if (this.initialized) return this;
 
             this.options = Object.assign({
-                stripMetaTags: true,
-                hookCreateElement: true,
-                hookPropertySetters: true,
-                hookAppendChild: true,
-                blockViolationReports: true,
-                verbose: true
+                verbose: true,
+                periodicCleanup: true,
+                cleanupDuration: 5000,
+                pollInterval: 20
             }, options || {});
 
             const opts = this.options;
 
-            if (opts.stripMetaTags) {
-                this._removeExistingMetaTags();
-                this._setupMetaObserver();
-                if (opts.verbose) console.log('[CSP Disabler] Meta tag watcher active');
+            // Hook setAttribute
+            const origSetAttr = Element.prototype.setAttribute;
+            Element.prototype.setAttribute = function(name, value) {
+                if (this.nodeName === 'META' && name.toLowerCase() === 'http-equiv' && /content-security-policy/i.test(value || '')) {
+                    if (opts.verbose) console.log('[CSP Disabler] BLOCKED setAttribute("http-equiv", CSP)');
+                    return;
+                }
+                return origSetAttr.call(this, name, value);
+            };
+
+            // Hook appendChild
+            const origAppend = Node.prototype.appendChild;
+            Node.prototype.appendChild = function(node) {
+                if (node && node.nodeName === 'META') {
+                    const equiv = (node.getAttribute('http-equiv') || node.httpEquiv || '').toLowerCase();
+                    if (equiv.includes('content-security-policy')) {
+                        if (opts.verbose) console.log('[CSP Disabler] BLOCKED appendChild(CSP meta)');
+                        return node;
+                    }
+                }
+                return origAppend.call(this, node);
+            };
+
+            // Hook insertBefore
+            const origInsert = Node.prototype.insertBefore;
+            Node.prototype.insertBefore = function(node, ref) {
+                if (node && node.nodeName === 'META') {
+                    const equiv = (node.getAttribute('http-equiv') || node.httpEquiv || '').toLowerCase();
+                    if (equiv.includes('content-security-policy')) {
+                        if (opts.verbose) console.log('[CSP Disabler] BLOCKED insertBefore(CSP meta)');
+                        return node;
+                    }
+                }
+                return origInsert.call(this, node, ref);
+            };
+
+            // Remove existing
+            const removeExisting = () => {
+                let count = 0;
+                document.querySelectorAll('meta[http-equiv]').forEach(m => {
+                    if ((m.getAttribute('http-equiv') || '').toLowerCase().includes('content-security-policy')) {
+                        m.remove();
+                        count++;
+                    }
+                });
+                if (count && opts.verbose) console.log('[CSP Disabler] Removed', count, 'existing CSP meta(s)');
+            };
+            removeExisting();
+
+            // Observer
+            const startObserver = () => {
+                const target = document.documentElement || document.head;
+                if (!target) return false;
+                this.observer = new MutationObserver(mutations => {
+                    mutations.forEach(m => m.addedNodes.forEach(node => {
+                        if (node.nodeName === 'META') {
+                            const equiv = (node.getAttribute('http-equiv') || '').toLowerCase();
+                            if (equiv.includes('content-security-policy')) {
+                                node.remove();
+                                if (opts.verbose) console.log('[CSP Disabler] Observer removed CSP meta');
+                            }
+                        }
+                    }));
+                }).observe(target, { childList: true, subtree: true });
+                return true;
+            };
+
+            if (!startObserver()) {
+                const wait = () => startObserver() || setTimeout(wait, 5);
+                wait();
             }
 
-            if (opts.hookCreateElement) {
-                this._hookCreateElement();
-                if (opts.verbose) console.log('[CSP Disabler] createElement hooked');
+            // Periodic cleanup
+            if (opts.periodicCleanup) {
+                const start = Date.now();
+                const tick = () => {
+                    removeExisting();
+                    if (Date.now() - start < opts.cleanupDuration) {
+                        this.cleanupTimer = setTimeout(tick, opts.pollInterval);
+                    }
+                };
+                tick();
             }
 
-            if (opts.hookPropertySetters) {
-                this._hookPropertySetters();
-                if (opts.verbose) console.log('[CSP Disabler] Property setters hooked');
-            }
-
-            if (opts.hookAppendChild) {
-                this._hookAppendChild();
-                if (opts.verbose) console.log('[CSP Disabler] appendChild hooked');
-            }
-
-            if (opts.blockViolationReports) {
-                this._blockViolationReports();
-                if (opts.verbose) console.log('[CSP Disabler] Violation reports blocked');
-            }
+            // Block violation reports
+            window.addEventListener('securitypolicyviolation', e => {
+                e.stopImmediatePropagation();
+                e.preventDefault();
+                if (opts.verbose) console.log('[CSP Disabler] Blocked violation report:', e.blockedURI);
+            }, true);
 
             this.initialized = true;
-            if (opts.verbose) console.log('[CSP Disabler] Initialized (meta-tag CSP only)');
+            if (opts.verbose) console.log('[CSP Disabler] Initialized');
 
             return this;
         },
 
-        _isCSPMeta: function(node) {
-            if (!node || node.nodeName !== 'META') return false;
-            const equiv = (node.getAttribute('http-equiv') || node.httpEquiv || '').toLowerCase();
-            return equiv.includes('content-security-policy');
-        },
-
-        _removeExistingMetaTags: function() {
-            const metaTags = document.querySelectorAll('meta[http-equiv]');
-            metaTags.forEach(meta => {
-                if (this._isCSPMeta(meta)) {
-                    meta.remove();
-                    if (this.options.verbose) console.log('[CSP Disabler] Removed CSP meta tag');
-                }
-            });
-        },
-
-        _setupMetaObserver: function() {
-            const self = this;
-            const tryObserve = () => {
-                const target = document.documentElement || document.head;
-                if (!target) return false;
-
-                this.observer = new MutationObserver((mutations) => {
-                    mutations.forEach((mutation) => {
-                        mutation.addedNodes.forEach((node) => {
-                            if (self._isCSPMeta(node)) {
-                                node.remove();
-                                if (self.options.verbose) console.log('[CSP Disabler] Blocked injected CSP meta tag');
-                            }
-                        });
-                    });
-                });
-
-                this.observer.observe(target, { childList: true, subtree: true });
-                return true;
-            };
-
-            if (!tryObserve()) {
-                const check = () => {
-                    if (tryObserve()) return;
-                    if (document.readyState !== 'complete') {
-                        setTimeout(check, 10);
-                    }
-                };
-                check();
-            }
-        },
-
-        _hookCreateElement: function() {
-            const self = this;
-            const originalCreateElement = document.createElement;
-
-            document.createElement = function(tagName) {
-                const element = originalCreateElement.call(document, tagName);
-
-                if (tagName.toLowerCase() === 'meta') {
-                    // Hook setAttribute
-                    const originalSetAttribute = element.setAttribute;
-                    element.setAttribute = function(name, value) {
-                        if (name.toLowerCase() === 'http-equiv' &&
-                            /content-security-policy/i.test(value || '')) {
-                            if (self.options.verbose) console.log('[CSP Disabler] Blocked meta.setAttribute CSP');
-                            return;
-                        }
-                        return originalSetAttribute.call(this, name, value);
-                    };
-
-                    // Hook httpEquiv property
-                    let httpEquivValue = '';
-                    Object.defineProperty(element, 'httpEquiv', {
-                        get: () => httpEquivValue,
-                        set: (value) => {
-                            if (/content-security-policy/i.test(value || '')) {
-                                if (self.options.verbose) console.log('[CSP Disabler] Blocked meta.httpEquiv CSP');
-                                return;
-                            }
-                            httpEquivValue = value;
-                        },
-                        configurable: true
-                    });
-                }
-
-                return element;
-            };
-        },
-
-        _hookPropertySetters: function() {
-            // Hook on <meta> elements created before our script ran
-            const self = this;
-            const originalDefineProperty = Object.defineProperty;
-
-            Object.defineProperty = function(obj, prop, descriptor) {
-                if (obj && obj.nodeName === 'META' && prop === 'httpEquiv') {
-                    const originalSet = descriptor.set;
-                    if (originalSet) {
-                        descriptor.set = function(value) {
-                            if (/content-security-policy/i.test(value || '')) {
-                                if (self.options.verbose) console.log('[CSP Disabler] Blocked Object.defineProperty CSP');
-                                return;
-                            }
-                            return originalSet.call(this, value);
-                        };
-                    }
-                }
-                return originalDefineProperty.call(this, obj, prop, descriptor);
-            };
-        },
-
-        _hookAppendChild: function() {
-            const self = this;
-            const nodes = [document.documentElement, document.head, document.body].filter(Boolean);
-
-            nodes.forEach((target) => {
-                if (!target._cspHooked) {
-                    const originalAppendChild = target.appendChild;
-                    target.appendChild = function(node) {
-                        if (self._isCSPMeta(node)) {
-                            if (self.options.verbose) console.log('[CSP Disabler] Blocked appendChild CSP meta');
-                            return node;
-                        }
-                        return originalAppendChild.call(this, node);
-                    };
-                    target._cspHooked = true;
-                }
-            });
-
-            // Also hook insertBefore
-            nodes.forEach((target) => {
-                if (!target._cspInsertHooked) {
-                    const originalInsertBefore = target.insertBefore;
-                    target.insertBefore = function(node, ref) {
-                        if (self._isCSPMeta(node)) {
-                            if (self.options.verbose) console.log('[CSP Disabler] Blocked insertBefore CSP meta');
-                            return node;
-                        }
-                        return originalInsertBefore.call(this, node, ref);
-                    };
-                    target._cspInsertHooked = true;
-                }
-            });
-        },
-
-        _blockViolationReports: function() {
-            window.addEventListener('securitypolicyviolation', (e) => {
-                e.stopImmediatePropagation();
-                e.preventDefault();
-                if (this.options.verbose) console.log('[CSP Disabler] Blocked violation report:', e.blockedURI);
-            }, true);
-        },
-
         destroy: function() {
-            if (this.observer) {
-                this.observer.disconnect();
-                this.observer = null;
-            }
+            if (this.observer) this.observer.disconnect();
+            if (this.cleanupTimer) clearTimeout(this.cleanupTimer);
             this.initialized = false;
             console.log('[CSP Disabler] Destroyed');
         }
     };
 
-    // Auto-init in userscript environment
-    if (typeof GM_info !== 'undefined' || typeof GM !== 'undefined' || typeof unsafeWindow !== 'undefined') {
+    // FIXED: Auto-init in any browser environment (not just userscript sandbox)
+    if (typeof window !== 'undefined' && window.document) {
         CSPDisabler.init();
     }
 
-    // Expose to global scope
     global.CSPDisabler = CSPDisabler;
 
-    // Module export
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = CSPDisabler;
     }
